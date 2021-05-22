14de9f93eeb06086b38ebd9bd7c83c638c90ea1c
Grant Shirley <grantmshirley516@gmail.com>
2021-05-22 15:00:27 -0600

    tested the code in another file and made a couple of fixes

--- game/jumper.py
+++ game/jumper.py
@@ -2,22 +2,23 @@
 class Jumper:
 
     def __init__(self) :
-        parachute = [" ___ "," /___\ ", " \   /  ", " \ / "," 0 "," /|\ ", " / \ "]
+        parachute = ["  ___ "," /___\ ", " \   /  ", "  \ / ","   0 ","  /|\ ", "  / \ "]
+
 
 
     def get_board(self):
 
-        for i in self.parachute:
-            print(self.parachute[i])
+        for i in range(0, len(self.parachute)):
+            print(f"{self.parachute[i]}")
 
-            return self.parachute
+            #return self.parachute
 
     def remove_pieces(self,guess_val):
         if guess_val == 3:
-            self.parachute.remove(" ___ ")
+            self.parachute.remove("  ___ ")
         elif guess_val == 2:
             self.parachute.remove(" /___\ ")
         elif guess_val == 1:
             self.parachute.remove(" \   /  ")
         elif guess_val == 0:
-            self.parachute.remove(" \ / ")
+            self.parachute.remove("  \ / ")
